dee8663a2142bb53e216d22eebd24ff07d4616f7
Iqtier Uddin Ahammad <iqt.taki@gmail.com>
2023-03-05 23:36:58 -0330

    Commiting for first time

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -3,9 +3,39 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { dispatch, budget, expenses } = useContext(AppContext);
+
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += item.cost);
+    }, 0);
+
+    const handleChange = (e) => {
+
+        let budget = e.target.value
+        if (budget>20001){
+            alert("The value cannot exceed remaining funds")
+        }else if(budget<totalExpenses){
+            alert('you cannot reduce the budget lower than the spending')
+        }else{
+            dispatch({
+                type: 'SET_BUDGET',
+                payload: budget,
+            });
+        };
+        }
+
     return (
         <div className='alert alert-secondary'>
+            <input
+                required='required'
+                type='number'
+                id='cost'
+                value={budget}
+                style={{ marginLeft: '2rem', size: 10 }}
+                onChange={handleChange}
+                step="10"
+
+            />
             <span>Budget: £{budget}</span>
         </div>
     );
