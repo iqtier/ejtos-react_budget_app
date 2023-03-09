81a29381dd1d9c62bc6473882a81abdae251cab0
Iqtier Uddin Ahammad <iqt.taki@gmail.com>
2023-03-08 20:44:27 -0330

    updated budget style

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -12,7 +12,7 @@ const Budget = () => {
     const handleChange = (e) => {
 
         let budget = e.target.value
-        if (budget>20001){
+        if (budget>2000){
             alert("The value cannot exceed remaining funds")
         }else if(budget<totalExpenses){
             alert('you cannot reduce the budget lower than the spending')
@@ -25,14 +25,14 @@ const Budget = () => {
         }
 
     return (
-        <div className='alert alert-secondary'>
-            <label htmlFor="cost">Budget :{currency} </label>
+        <div className='alert alert-secondary' style = {{display: 'flex', justifyContent: 'left'}}>
+            <label htmlFor="cost"  style={{display:'inline-block',  width: 100}}>Budget : {currency}</label>
             <input
                 required='required'
                 type='number'
                 id='cost'
                 value={budget}
-                style={{ marginLeft: '2rem', size: 10 }}
+                style={{ display:'inline-block', marginLeft:-22}}
                 onChange={handleChange}
                 step="10"
 
